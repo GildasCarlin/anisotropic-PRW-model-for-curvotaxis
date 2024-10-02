8b12a75a047eddb39cd5d360b0998c7f4b43d18b
GildasCarlin <127842813+GildasCarlin@users.noreply.github.com>
2024-10-02 12:14:52 +0200

    Update curvature.js

--- curvature.js
+++ curvature.js
@@ -1,4 +1,4 @@
-// curvature.js is a package writtent by Gildas Carlin
+// curvature.js is a package writtent by Gildas Carlin, Ian Manifacier and Jean-Louis Milan
 // analytical results for curvature measurements
 //
 
