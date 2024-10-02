a62cf0d5f7b7a77746700613cea343d627314ac3
GildasCarlin <127842813+GildasCarlin@users.noreply.github.com>
2024-10-02 12:17:18 +0200

    Update drawarrow.js

--- drawarrow.js
+++ drawarrow.js
@@ -1,4 +1,4 @@
-// drawarrow.js is a package writtent by Gildas Carlin, Ian Manifacier and Jean-Louis Milan
+// drawarrow.js is a package written by Gildas Carlin, Ian Manifacier and Jean-Louis Milan
 // draw an arrow
 //
 
