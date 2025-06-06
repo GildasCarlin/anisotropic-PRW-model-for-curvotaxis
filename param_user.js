a6a2ee59007ad271d00eff265a2dfc6b3fb1903c
GildasCarlin <127842813+GildasCarlin@users.noreply.github.com>
2025-06-06 11:35:44 +0200

    Update param_user.js
    add doi

--- param_user.js
+++ param_user.js
@@ -1,7 +1,7 @@
 
 /**
  * File              : param_user.js
- * Original article  : doi
+ * Original article  : doi: 10.1038/s41598-025-02804-3
  * Authors           : Gildas Carlin, Ian Manifacier and Jean-Louis Milan
  * Date              : 05/05/2025
  * Description       : User interface.
